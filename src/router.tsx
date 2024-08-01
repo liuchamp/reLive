764cb89c9c973c5375f9482acd61a8807d398df1
LiuChamp <2756154878@qq.com>
2024-08-02 00:07:52 +0800

    加载router

--- src/router.tsx
+++ src/router.tsx
@@ -2,39 +2,39 @@ import {
     useRoutes,
 } from 'react-router-dom'
 
-// import ErrorPage from "./error-page";
-// import MainLayout from "./components/layout/OuterLayout";
+import ErrorPage from "./error-page";
+import MainLayout from "./components/layout/OuterLayout";
 
 import routes from '~react-pages'
 import { Suspense } from 'react';
 
-// const AppRoutes = () => {
-//     const element = useRoutes([
-//         {
-//             path: '/',
-//             element: <MainLayout />,
-//             errorElement: <ErrorPage />,
-//             children: routes,
-//         },
-//         // {
-//         //     path: '/admin',
-//         //     element: <AdminLayout />,
-//         //     children: routes.filter(route => route.path.startsWith('/admin')),
-//         // },
-//     ]);
-//     console.log(element)
-//     return (
-//         <Suspense fallback={<p>Loading...</p>}>
-//             {element}
-//         </Suspense>
-//     )
-// };
-
-function AppRoutes() {
+const AppRoutes = () => {
+    const element = useRoutes([
+        {
+            path: '/',
+            element: <MainLayout />,
+            errorElement: <ErrorPage />,
+            children: routes,
+        },
+        // {
+        //     path: '/admin',
+        //     element: <AdminLayout />,
+        //     children: routes.filter(route => route.path.startsWith('/admin')),
+        // },
+    ]);
+    console.log(element)
     return (
         <Suspense fallback={<p>Loading...</p>}>
-            {useRoutes(routes)}
+            {element}
         </Suspense>
     )
-}
+};
+
+// function AppRoutes() {
+//     return (
+//         <Suspense fallback={<p>Loading...</p>}>
+//             {useRoutes(routes)}
+//         </Suspense>
+//     )
+// }
 export default AppRoutes
